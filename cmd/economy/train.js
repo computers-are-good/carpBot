07a93cc08cd7c6f544fe4cead4eec2a56557572a
brick <weiyij@protonmail.com>
2025-01-03 20:17:47 +1300

    /train

---
+++ cmd/economy/train.js
@@ -0,0 +1,53 @@
+const { SlashCommandBuilder } = require('discord.js');
+const fs = require("fs");
+const path = require('node:path');
+const economyUtils = require(path.join(__dirname, "../../utils/economy"));
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('train')
+		.setDescription('Spend some money to improve your stats in combat!')
+		.addStringOption(option =>
+			option.setName('stat')
+				.setDescription('The stat to improve')
+				.setRequired(true)
+				.addChoices(
+					{ name: 'Health', value: 'maxHealth' },
+					{ name: 'Attack', value: 'attack' },
+					{ name: 'Block', value: 'block' },
+			)),
+	async execute(interaction) {
+		userInfo = await economyUtils.prefix(interaction);
+		const category = interaction.options.getString('stat');
+		const cost = Math.max(userInfo.abilitiesImproved[category] * 500, 100) * 100;
+		let statToImproveReadable = category;
+		if (category == "maxHealth") statToImproveReadable = "max health";
+		let valueToIncrease;
+		switch (category) {
+			case "maxHealth":
+				valueToIncrease = 10;
+				break;
+			case "attack":
+				valueToIncrease = 2;
+				break;
+			case "block":
+				valueToIncrease = 1;
+				break;
+		}
+		const val = await economyUtils.confirmation(interaction, `You will increase ${statToImproveReadable} (${userInfo.combat[category]} -> ${userInfo.combat[category] + valueToIncrease}). This will cost ${economyUtils.formatMoney(cost)}. Are you sure?`);
+		const { confirmed, response } = val;
+		if (confirmed) {
+			if (userInfo.moneyOnHand < cost) {
+				response.edit("You do not have enough money to do training.");
+				return;
+			}
+			userInfo.moneyOnHand -= cost;
+			userInfo.abilitiesImproved[category]++;
+			userInfo.combat[category] += valueToIncrease;
+			response.edit(`Increased ${statToImproveReadable} (${userInfo.combat[category] - valueToIncrease} -> ${userInfo.combat[category]}).`)
+			fs.writeFileSync(path.join(__dirname, `../../userdata/${interaction.user.id}`), JSON.stringify(userInfo));
+		} else {
+			response.edit("Training cancelled.");
+		}
+	},
+};
