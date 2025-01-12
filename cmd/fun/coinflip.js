2c27bdc3cbf9c5371e3927e6e898ea0c15743959
brick <weiyij@protonmail.com>
2025-01-12 22:46:22 +1300

    add images to /coinflip

--- cmd/fun/coinflip.js
+++ cmd/fun/coinflip.js
@@ -1,9 +1,15 @@
 const { SlashCommandBuilder } = require('discord.js');
+const path = require('node:path');
+//images from https://www.rbnz.govt.nz/money-and-cash/coins/coins-in-circulation/coin-specifications-and-images-by-denomination
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('coinflip')
 		.setDescription('Flip a coin!'),
 	async execute(interaction) {
-		await interaction.reply(Math.random() < 0.5 ? "Heads" : "Tails");
+		let rand = Math.random();
+		await interaction.reply({
+			content: rand < 0.5 ? "Heads" : "Tails",
+			files: [{ attachment: path.join(__dirname, `../../data/${rand < 0.5 ? "heads" : "tails"}.jpg`) }]
+		});
 	},
 };
