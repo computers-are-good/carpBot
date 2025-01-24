9b4ae33c2914888ecc6e785bf330427f761e6f96
brick <weiyij@protonmail.com>
2025-01-25 11:54:01 +1300

    refactor dungeon.js

--- utils/dungeon.js
+++ utils/dungeon.js
@@ -5,8 +5,31 @@ const { createUserData } = require(path.join(__dirname, "/createUserData"));
 const scriptingUtils = require(path.join(__dirname, "/scripting"));
 const { monsters } = require(path.join(__dirname, "../data/monsters"));
 const economyUtils = require(path.join(__dirname, "/economy"));
+function battle(player, enemy) {
+    function attack(dmg, target) {
+        target.shield -= dmg;
+        if (target.shield < 0) {
+            target.health += target.shield;
+            target.shield = 0;
+        }
+        return target;
+    }
 
+    while (true) {
+        player.shield = player.block;
+        enemy.shield = enemy.block;
+        attack(player.attack, enemy);
+        attack(enemy.attack, player);
+        if (player.health <= 0) {
+            player.health = 0;
+            return false;
+        } else if (enemy.health <= 0) {
+            return true;
+        }
+    }
+}
 module.exports = {
+    battle,
     dungeon: async function (interaction, script, userInfo) {
         return new Promise(async (res, rej) => {
             let weAreFinished = false;
@@ -73,36 +96,6 @@ ${enemyStats.block > playerStats.attack ? "The enemy has higher block than your 
                 enemyStats.img = monsters[enemyName].img;
                 return enemyStats;
             }
-            function attack(dmg, target) {
-                target.shield -= dmg;
-                if (target.shield < 0) {
-                    target.health += target.shield;
-                    target.shield = 0;
-                }
-                return target;
-            }
-            function battle(itemIndex) {
-                const enemyName = script[itemIndex].content.name;
-
-                if (!(enemyName in monsters)) {
-                    throw new Error("Enemy not found!")
-                }
-
-                const enemyStats = generateEnemyStats(itemIndex);
-
-                while (true) {
-                    playerStats.shield = playerStats.block;
-                    enemyStats.shield = enemyStats.block;
-                    attack(playerStats.attack, enemyStats);
-                    attack(enemyStats.attack, playerStats);
-                    if (playerStats.health <= 0) {
-                        playerStats.health = 0;
-                        return false;
-                    } else if (enemyStats.health <= 0) {
-                        return true;
-                    }
-                }
-            }
             let response;
             let results = processCurrentIndex(itemIndex);
             let stringToReply = results.string;
@@ -145,7 +138,8 @@ ${enemyStats.block > playerStats.attack ? "The enemy has higher block than your 
                             });
                         } else {
                             if (script[itemIndex].type == "battle") {
-                                let won = battle(itemIndex);
+                                const enemyStats = generateEnemyStats(itemIndex);
+                                let won = battle(playerStats, enemyStats);
                                 if (!won) {
                                     await buttons.update({ content: "Lost battle. Exiting dungeon...", components: [] });
                                     await scriptingUtils.wait(2000);
