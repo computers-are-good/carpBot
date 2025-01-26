55639ef9340777d7fe34c93b478ed530e91d1469
brick <weiyij@protonmail.com>
2025-01-27 11:48:31 +1300

    fix award money when levelling up

--- utils/levelup.js
+++ utils/levelup.js
@@ -25,14 +25,20 @@ module.exports = {
         let oldLevel = userInfo.level;
         userInfo.level = results.newLevel;
         userInfo.expRequired = results.newExpRequired;
-        userInfo.moneyOnHand += userInfo.level * 10000;
+        let moneyAwarded = 0;
+        if (oldLevel !== userInfo.level) {
+            for (let i = oldLevel + 1; i < userInfo.level + 1; i++) {
+                moneyAwarded += i * 10000;
+            }
+            userInfo.moneyOnHand += moneyAwarded;
+        }
         userInfo.combat.attack += 2;
         userInfo.combat.maxHealth += 10;
         userInfo.combat.block += 1;
         userInfo.combat.speed += 5;
 
         if (oldLevel !== results.newLevel) {
-            return `Congratulations! You levelled up (${oldLevel} -> ${userInfo.level}). Your stats are increased. Level up bonus: $${userInfo.level * 100}.`;
+            return `Congratulations! You levelled up (${oldLevel} -> ${userInfo.level}). Your stats are increased. Level up bonus: $${moneyAwarded / 100}.`;
         }
         return `You gained ${expToGain} experience. You need ${userInfo.expRequired} experience to level up (${Math.round((1 - (userInfo.expRequired / expRequiredToLevelUp(userInfo.level))) * 1000) / 10}% there).`;
     }
